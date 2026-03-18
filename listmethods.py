ac2220dcd2f6df679e0c8bcf66c25edb47e63a1b
vishnu teja <vishnutejakorada@gmail.com>
2026-03-18 12:41:26 +0530

    C:\Users\vishn\OneDrive\Desktop\python 10k.py

--- listmethods.py
+++ listmethods.py
@@ -6,18 +6,44 @@
 # print(l1.pop(-1))
 
 
+#copy
+#shallowcopy
+# l1=["hi","friends","maku",["pandagaledu","friends"]]
+# l2=l1.copy()
+# l1.append(1)
+# l2.append(4)
+# print(l1)
+# print(l2)
+# print(id(l1))
+# print(id(l2))
+# l1.append(4)
+# l1[3].append("prends")
+# print(l1)
+# print(l2)
+# print(id(l1[3]))
+# print(id(l2[3]))
 
-l1=["hi","friends","maku",["pandagaledu","friends"]]
-l2=l1.copy()
-l1.append(1)
-l2.append(4)
+#deepcopy
+# from copy import deepcopy
+l1=[1,2,3,4,[5,6,7]]
+# l2=deepcopy(l1)
+# print("outer")
+# print(l1)
+# print(l2)
+# print(id(l1))
+# print(id(l2))
+l1.append("sal ram")
+# l2.append("friends")
+# print("inner")
+# print(l1)
+# print(l2)
+# print(id(l1[-1]))
+# print(id(l2[-1]))
+
+#packing and unpacking 
+l1=[1,2,3,4,5]
+a,b,*c=l1
 print(l1)
-print(l2)
-print(id(l1))
-print(id(l2))
-l1.append(4)
-l1[3].append("prends")
-print(l1)
-print(l2)
-print(id(l1[3]))
-print(id(l2[3]))
+print(a)
+print(b)
+print(c)
